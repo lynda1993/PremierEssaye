b775a03cd1b710ea31c776f446dc2a33735d4fc9
lynda <lynda.abizar@gmail.com>
2018-02-13 09:19:35 +0100

    premier commit

---
+++ ProjetGit/src/com/Class/Principal.java
@@ -0,0 +1,12 @@
+package com.Class;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 System.out.println("mon premier commit");
+		 		
+	}
+
+}
